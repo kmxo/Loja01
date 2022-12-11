52f3244aa18fe98ede9c39cd35800e0604a05ad6
kmxo <c_olavo@hotmail.com>
2022-12-11 17:55:43 +0000

    new files

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,33 @@
+﻿using Loja01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Loja01.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            ClientViewModel client = new ClientViewModel()
+            {
+                Name = "Carlos Camacho",
+                Email = "carlos.camacho@email.com",
+                Phone = "+351 345 678 901"
+
+            };
+
+            return View(client);
+        }
+
+        public IActionResult Create()
+        {
+            var client = new ClientViewModel();
+
+            return View(client);
+        }
+
+        public IActionResult CreateClient(ClientViewModel client)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
